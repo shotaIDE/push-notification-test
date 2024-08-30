662c06c1fbae6b7f12b04bd22f3dc9d393439752
Shota Ide <ide.shota.dev@gmail.com>
2024-08-31 00:47:35 +0900

    Initial commit

---
+++ ios/iOSPushNotificationTest/iOSPushNotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPushNotificationTestApp.swift
+//  iOSPushNotificationTest
+//
+//  Created by Shota Ide on 2024/08/31.
+//
+
+import SwiftUI
+
+@main
+struct iOSPushNotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

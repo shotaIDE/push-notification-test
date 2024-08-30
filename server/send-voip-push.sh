8f06089ffa180b68673fea1b3039ecf13bfc16f0
Shota Ide <ide.shota.dev@gmail.com>
2024-08-31 03:16:09 +0900

    feat: send push notifications by shell

---
+++ server/send-voip-push.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source ./.env
+
+if [ USE_SANDBOX == 1 ]; then
+    ENDPOINT='https://api.development.push.apple.com'
+else
+    ENDPOINT='https://api.push.apple.com'
+fi
+
+read -r -d '' payload <<-'EOF'
+{
+    "aps": {
+        "title": "Test Message",
+        "body": "This push notification was sent by requesting APNs directly at TIME"
+    }
+}
+EOF
+
+base64() {
+   openssl base64 -e -A | tr -- '+/' '-_' | tr -d =
+}
+
+sign() {
+   printf "$1"| openssl dgst -binary -sha256 -sign "$APNS_AUTH_KEY_FILE_PATH" | base64
+}
+
+time=$(date +%s)
+header=$(printf '{ "alg": "ES256", "kid": "%s" }' "$APNS_AUTH_KEY_ID" | base64)
+claims=$(printf '{ "iss": "%s", "iat": %d }' "$TEAM_ID" "$time" | base64)
+jwt="$header.$claims.$(sign $header.$claims)"
+
+time_str=$(date -Iseconds)
+curl --verbose \
+   --header "content-type: application/json" \
+   --header "authorization: bearer $jwt" \
+   --header "apns-topic: ${BUNDLE_ID}.voip" \
+   --data "${payload/TIME/$time_str}" \
+   $ENDPOINT/3/device/$VOIP_DEVICE_TOKEN

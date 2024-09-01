3892d053c8afb97cfa132552bd6ee711eda23d82
Shota Ide <ide.shota.dev@gmail.com>
2024-09-01 14:46:08 +0900

    refactor

--- server/apns.py
+++ server/apns.py
@@ -36,39 +36,24 @@ def __get_jwt_token():
 # https://developer.apple.com/documentation/usernotifications/sending-notification-requests-to-apns
 
 
-async def send_user_notification():
-    print('Sending user notification...')
-
-    BUNDLE_ID = os.environ.get('BUNDLE_ID')
-    DEVICE_TOKEN = os.environ.get('DEVICE_TOKEN')
+async def __send_request(device_tokens: list[str], apns_push_type: str, apns_topic: str, aps_payload: dict):
     USE_SANDBOX = os.environ.get('USE_SANDBOX')
+
     if USE_SANDBOX == '1':
         HOST = 'api.sandbox.push.apple.com'
     else:
         HOST = 'api.push.apple.com'
 
-    token = __get_jwt_token()
-
-    device_tokens = [
-        DEVICE_TOKEN
-    ]
+    jwt_token = __get_jwt_token()
 
     headers = {
-        'authorization': f'bearer {token}',
-        'apns-push-type': 'alert',
-        'apns-topic': f'{BUNDLE_ID}'
+        'authorization': f'bearer {jwt_token}',
+        'apns-push-type': apns_push_type,
+        'apns-topic': apns_topic
     }
 
-    current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
     payload = {
-        'aps': {
-            'alert': {
-                'title': 'Test Title (via APNs with Python)',
-                'sound': 'default',
-                'body': f'This user notification was sent by requesting APNs directly at {current_datetime}'
-            }
-        }
+        'aps': aps_payload
     }
 
     async with httpx.AsyncClient(http2=True) as client:
@@ -87,57 +72,54 @@ async def send_user_notification():
                 f'#{padded_index} response status code: '
                 f'{response.status_code}'
             )
+
+
+async def send_user_notification():
+    print('Sending user notification...')
+
+    DEVICE_TOKEN = os.environ.get('DEVICE_TOKEN')
+    BUNDLE_ID = os.environ.get('BUNDLE_ID')
+
+    device_tokens = [
+        DEVICE_TOKEN
+    ]
+
+    apns_push_type = 'alert'
+    apns_topic = BUNDLE_ID
+
+    current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    aps_payload = {
+        'alert': {
+            'title': 'Test Title (via APNs with Python)',
+            'sound': 'default',
+            'body': f'This user notification was sent by requesting APNs directly at {current_datetime}'
+        }
+    }
+
+    await __send_request(device_tokens, apns_push_type, apns_topic, aps_payload)
 
 
 # https://developer.apple.com/documentation/usernotifications/sending-notification-requests-to-apns
 async def send_voip_push_notification():
     print('Sending VoIP notification...')
 
-    BUNDLE_ID = os.environ.get('BUNDLE_ID')
     DEVICE_TOKEN = os.environ.get('VOIP_DEVICE_TOKEN')
-    USE_SANDBOX = os.environ.get('USE_SANDBOX')
-    if USE_SANDBOX == '1':
-        HOST = 'api.sandbox.push.apple.com'
-    else:
-        HOST = 'api.push.apple.com'
-
-    token = __get_jwt_token()
+    BUNDLE_ID = os.environ.get('BUNDLE_ID')
 
     device_tokens = [
         DEVICE_TOKEN
     ]
 
-    headers = {
-        'authorization': f'bearer {token}',
-        'apns-push-type': 'voip',
-        'apns-topic': f'{BUNDLE_ID}.voip'
-    }
+    apns_push_type = 'voip'
+    apns_voip_topic = f'{BUNDLE_ID}.voip'
 
     current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    payload = {
-        'aps': {
-            'title': 'Test Title (via APNs with Python)',
-            'body': f'This VoIP notification was sent at {current_datetime}'
-        }
+    apns_payload = {
+        'title': 'Test Title (via APNs with Python)',
+        'body': f'This VoIP notification was sent at {current_datetime}'
     }
 
-    async with httpx.AsyncClient(http2=True) as client:
-        for index, device_token in enumerate(device_tokens):
-            url = f'https://{HOST}/3/device/{device_token}'
-
-            response = await client.post(
-                url,
-                headers=headers,
-                json=payload
-            )
-
-            padded_index = str(index).rjust(2, '0')
-            print(f'#{padded_index} device token = {device_token}')
-            print(
-                f'#{padded_index} response status code: '
-                f'{response.status_code}'
-            )
+    await __send_request(device_tokens, apns_push_type, apns_voip_topic, apns_payload)
 
 
 if __name__ == '__main__':

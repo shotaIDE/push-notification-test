6a2018feb570cbb0a9c44cffa96aca060125904a
Shota Ide <ide.shota.dev@gmail.com>
2024-09-07 17:20:46 +0900

    fix

--- server/test_apns.py
+++ server/test_apns.py
@@ -5,7 +5,7 @@ import os
 
 import pytest
 
-from apns import send_user_notification
+from apns import send_user_notification, send_voip_push_notification
 
 
 @pytest.mark.asyncio
@@ -20,4 +20,19 @@ async def test_send_user_notification(capfd):
         'Sending user notification...\n'
         f'#00 device token = {device_token}\n'
         '#00 response status code: 410\n'
+    )
+
+
+@pytest.mark.asyncio
+async def test_send_voip_push_notification(capfd):
+    device_token = os.environ.get('VOIP_DEVICE_TOKEN')
+
+    await send_voip_push_notification()
+
+    out, _ = capfd.readouterr()
+
+    assert out == (
+        'Sending VoIP notification...\n'
+        f'#00 device token = {device_token}\n'
+        '#00 response status code: 200\n'
     )

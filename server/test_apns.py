f76876b89aee83911b55728508328f798fe81e07
Shota Ide <ide.shota.dev@gmail.com>
2024-09-07 12:00:16 +0900

    pytest

---
+++ server/test_apns.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+
+import os
+
+import pytest
+
+from apns import send_user_notification
+
+
+@pytest.mark.asyncio
+async def test_send_user_notification(capfd):
+    device_token = os.environ.get('DEVICE_TOKEN')
+
+    await send_user_notification()
+
+    out, _ = capfd.readouterr()
+
+    assert out == (
+        'Sending user notification...\n'
+        f'#00 device token = {device_token}\n'
+        '#00 response status code: 200\n'
+    )
